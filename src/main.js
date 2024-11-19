c66a8bda3cb542026392a9a0eaf75bcab2299766
Aaron Santos <2302042@utrivieramaya.edu.mx>
2024-11-19 18:26:32 -0500

    feat: Crear las bases del Sign-in y Register
    Se descargan los paquetes oficiales desde npm. Vue-router y firebase para
    crear redireccionamientos y autenticaciones en la app, ádemas de crear
    el HTML y estilos de la web (footer y navbar) y la conexión a Firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./router"
 
-createApp(App).mount('#app')
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWjk_xzokxHQVjgW_vnhqca2D9sEa6wOw",
+  authDomain: "login-app-prueba-1.firebaseapp.com",
+  projectId: "login-app-prueba-1",
+  storageBucket: "login-app-prueba-1.firebasestorage.app",
+  messagingSenderId: "809691277226",
+  appId: "1:809691277226:web:098f4df339f4d74b1a2dd3"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
